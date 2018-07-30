3286ee34b4e4e10bda241310570bb2f3a821666f
SandraCGarcia <sandra.gcanete@gmail.com>
2018-07-30 09:31:48 +0200

    first commit

---
+++ src/SheepCounter.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class SheepCounter extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1></h1>
+        <button>Pulsar</button>
+      </div>
+    );
+  }
+}
+
+export default SheepCounter;
